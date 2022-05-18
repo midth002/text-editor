f5bb288765b8e465a4c76c0a3e09efdbba7b3d6d
Andrew Midthun <midth002@gmail.com>
2022-05-18 09:49:12 -0500

    Added to the putDb function to update indexedDB on client side.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
 
+    console.log('PUT to the database');
+    const jateDb = await openDB('jate', 1); 
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const req = store.put({contents: content});
+    const res = await req;
+    console.log('Data saved to the database', res)
+
+
+}
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
